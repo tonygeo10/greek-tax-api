e794543ecde6ec4323170e226454f22d9069073a
tonygeo10 <georgiouantony81@gmail.com>
2026-02-28 11:43:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CORS(app, resources={
 
 def get_db_connection():
     db_url = os.environ.get("DATABASE_URL")
-    conn = psycopg2.connect(db_url, sslmode="require")
+    conn = psycopg2.connect(db_url)
     return conn
 
 @app.route("/")
